2512315a1031d92fefd037f88d1db0160e1230a6
Marcus Kim <marcus.thinking@gmail.com>
2021-07-24 23:16:00 +0900

    modified js

--- JSON-start/js/scripts.js
+++ JSON-start/js/scripts.js
@@ -2,4 +2,11 @@ fetch ("testdata.json")
 .then((mydata) => mydata.json())
 .then((mydata) => {
     console.log(mydata);
+    console.log(mydata.phone);
+
+    document.getElementById("puppy").innerHTML = mydata.name;
+
+    let sample = document.createElement("h1");
+    sample.textContent = mydata.name;
+    document.getElementById("puppy").appendChild(sample);
 }); // end of .then
