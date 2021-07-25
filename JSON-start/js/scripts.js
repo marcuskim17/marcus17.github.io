b90d35245674c48cb27075d111d3d82777144419
Marcus Kim <marcus.thinking@gmail.com>
2021-07-25 15:04:20 +0900

    added api

--- JSON-start/js/scripts.js
+++ JSON-start/js/scripts.js
@@ -1,10 +1,15 @@
-fetch ("testdata.json")
-.then((mydata) => mydata.json())
-.then((mydata) => {
-    console.log(mydata);
-    console.log(mydata.phone);
+//ADD the key and change units to imperial
+const apiURL = "api.openweathermap.org/data/2.5/weather?id=5816861&appid=c34f810a4f43f048c6c18a7555bf8976&units=imperial"
 
-    document.getElementById("puppy").innerHTML = mydata.name;
+//Go fetch it and then wait for a response.
+fetch (apiURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+      //Once it comes back, display it to the console.
+
+    console.log(weatherInfo);
+
+    document.getElementById('place').innerHTML = weatherInfo.name;
 
     let sample = document.createElement("h1");
     sample.textContent = mydata.name;
