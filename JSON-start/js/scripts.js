7046e3e8f9aa5716c23db3ada977624845ca8456
Marcus Kim <marcus.thinking@gmail.com>
2021-08-29 14:50:36 +0900

    added javascript lines

--- JSON-start/js/scripts.js
+++ JSON-start/js/scripts.js
@@ -1,23 +1,60 @@
+const d = new Date();
+
+const todayDayNumber = d.getDay();
+
+const weekday = new Array[7];
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
 //ADD the key and change units to imperial
-const apiURL = "api.openweathermap.org/data/2.5/weather?id=5816861&appid=c34f810a4f43f048c6c18a7555bf8976&units=imperial"
+const apiURL = "//api.openweathermap.org/data/2.5/forecast?id=5879400&appid=c34f810a4f43f048c6c18a7555bf8976&units=imperial"
 
 //Go fetch it and then wait for a response.
 fetch (apiURL)
   .then((response) => response.json())
   .then((weatherInfo) => {
       //Once it comes back, display it to the console.
-
     console.log(weatherInfo);
 
-    document.getElementById('place').innerHTML = weatherInfo.name;
-    document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp;
-    document.getElementById('windSpeed').innerHTML = weatherInfo.wind.speed;
-    
-    const iconcode = weatherInfo.weather[0].icon;
-    console.log(iconcode);
-    const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
-    console.log(icon_path);
+    document.getElementById("townName").textContent = weatherInfo.city.name;
 
-    document.getElementById('weather_icon').src = icon_path;
+    let mylist = weatherInfo.list;
 
-}); // end of .then
+      let forecastDayNaumber = todayDayNumber;
+
+      for (i = 0; i < mylist.length; i++) {
+
+        let time = mylist[i].dt_txt;
+
+        if (time.includes("21:00:00")) {
+          console.log("Found an entry with 21:00:00 in the time. It was report "+i+" from the mylist of 40");
+
+          forecastDayNaumber += 1;
+          if (forecastDayNaumber === 7){forecastDayNaumber - 0;}
+
+          let theDayName = document.createElement["span"];
+          theDayName.textContent = weekday[forecastDayNaumber];
+
+          let theTemp = document.createElement["p"];
+          theTemp.textContent = weatherInfo.list[i].main.temp + "\xB0";
+
+          let iconcode = weatherInfo.list[i].weather[0].icon;
+          let iconPath = "//openweathermap.org/img/w/" + iconcode + ".png";
+          let theIcon = document.createElement("img");
+          theIcon.src = iconPath;
+
+          let theDay = document.createElement["div"];
+          theDay.appendChild(theDayName);
+          theDay.appendChild(theTemp);
+          theDay.appendChild(theIcon);
+
+          document.getElementById('weatherforcast').appendChild(theDay);
+
+        } // end if
+      }  // end for
+}); // end of "then" fat arrow function
